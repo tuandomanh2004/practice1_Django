761680ac666b3c265301d1722a398bde21341bc0
tuan do <tuandomanh2004@gmail.com>
2024-11-13 17:18:25 +0700

    add product-update module

--- storemanagement/management/urls.py
+++ storemanagement/management/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ShowProducts , show_detail ,ProductCreateView,home
+from .views import ShowProducts,ProductCreateView,ProductUpdateView,home,show_detail 
 urlpatterns = [
     path('' , home , name = 'home' ),
     path('product/showproducts/' , ShowProducts.as_view() , name = 'show_products'),
+    path('product/new/' , ProductCreateView.as_view() , name = 'product_new'),
     path('product/<int:pk>' , show_detail , name = 'product_detail'),
-    path('product/new/' , ProductCreateView.as_view() , name = 'product_new')
+    path('product/<int:pk>/edit' , ProductUpdateView.as_view() , name = 'product_edit' )
 ]
--- storemanagement/management/views.py
+++ storemanagement/management/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DetailView , CreateView
+from django.views.generic import ListView , DetailView , CreateView , DeleteView , UpdateView
 from .models import Product , Category , Review
 # Create your views here.
-class ShowProducts(ListView) : 
-    model = Product
-    template_name = "show_products.html"
-    context_object_name = 'all_products'
 def home(request) : 
     return render(request , 'base.html')
-# class ProductDetail(DetailView) : 
-#     model = Product
-#     template_name = "product_detail.html"
-#     context_object_name = 'product'
 def show_detail(request , pk) : 
     product = Product.objects.get(pk = pk)
     review = Review.objects.filter(product = product) 
-    return render(request , 'product_detail.html' , {'product' : product , 'review' : review})    
+    return render(request , 'product_detail.html' , {'product' : product , 'review' : review})  
+class ShowProducts(ListView) : 
+    model = Product
+    template_name = "show_products.html"
+    context_object_name = 'all_products'  
 class ProductCreateView(CreateView) : 
     model = Product
     template_name = 'new_product.html'
     fields = '__all__'
-
+class ProductUpdateView(UpdateView) :
+    model = Product
+    template_name = 'update_product.html'
+    fields = '__all__'
